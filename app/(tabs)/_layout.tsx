3e2006e04b5c76ec0e00855c72e3550af9a521e8
pookie-bear11 <ethangabrieldelapena@gmail.com>
2026-08-17 14:04:51 -0800

    Module 4

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -64,6 +64,22 @@ export default function TabLayout() {
           ),
         }}
       />
+
+      <Tabs.Screen
+  name="teacher"
+  options={{
+    title: 'Teacher',
+    tabBarIcon: ({ color, focused }) => (
+      <Ionicons
+        name={focused ? 'clipboard' : 'clipboard-outline'}
+        color={color}
+        size={24}
+      />
+    ),
+  }}
+/>
+
+
     </Tabs>
   );
 }
